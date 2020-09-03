16c15769eda5bf7f2e080afc77f8fcc6372064bb
goonzu <68987579+goonzu@users.noreply.github.com>
2020-09-04 05:35:00 +0900

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,12 @@
+const GoonzuInfo ={ 
+	favFood: [ 
+        { name: "Kimchi", fatty : true},
+		{ name : "Cheese burger", fatty : false} ] 
+	}
+
+    console.log(GoonzuInfo.favFood[1].fatty);
+
+// GoonzuInfo -> 객체
+// favFood -> 변수
+// name, fatty -> 변수
+// kimchi, true, Cheese burger, fatty -> kind of data
